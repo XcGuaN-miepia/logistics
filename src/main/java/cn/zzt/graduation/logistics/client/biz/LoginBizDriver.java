fd729d5ffd64aefea0164f704cb941b86a918476
zjzzt1996 <970695436@qq.com>
2018-03-03 23:06:00 +0800

    loginbiz

---
+++ src/main/java/cn/zzt/graduation/logistics/client/biz/LoginBizDriver.java
@@ -0,0 +1,23 @@
+package cn.zzt.graduation.logistics.client.biz;
+
+import com.google.gwt.user.client.Window;
+import com.google.web.bindery.event.shared.HandlerRegistration;
+
+import cn.easysw.mobileframework.client.framework.XGinjector;
+import cn.xiaobu.web.citybeauty.client.ajax.AjaxAsyncCallback;
+import cn.xiaobu.web.citybeauty.client.framework.BizDriverAbsImpl;
+
+public class LoginBizDriver extends BizDriverAbsImpl{
+
+	@Override
+	public HandlerRegistration addHandlerRegistration() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void dispath() {
+
+	}
+
+}
