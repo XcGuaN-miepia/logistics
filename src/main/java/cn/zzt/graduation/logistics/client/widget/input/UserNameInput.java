f9b29d2140e9d85ff79b4bde1aa79a8a8e576f0b
zjzzt1996 <970695436@qq.com>
2018-03-27 09:21:14 +0800

    widget

---
+++ src/main/java/cn/zzt/graduation/logistics/client/widget/input/UserNameInput.java
@@ -0,0 +1,23 @@
+package cn.zzt.graduation.logistics.client.widget.input;
+/**
+ * 账号输入框
+ * 
+ * @author zhangtao.zhou
+ * @version 2018年3月16日下午3:23:09
+ */
+public class UserNameInput extends AbstractBaseInput{
+
+	public UserNameInput() {
+		super();
+		
+		
+	}
+	
+	@Override
+	public boolean valid() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
+
